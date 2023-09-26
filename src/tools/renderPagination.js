f4c80961c5aa55bf57bd3512b3504e0de9cdf0aa
Chiheb Moussi <chihebmoussi@gmail.com>
2023-09-26 19:40:30 +0100

    add pagination

--- src/tools/renderPagination.js
+++ src/tools/renderPagination.js
@@ -6,14 +6,54 @@ export const renderPagination = (info, name="", page="") => {
     const nextPage = info.next ? currentPage + 1 : '' ;
     const prevPage = info.prev ? currentPage - 1 : '';
 
+
+    let pageNUmbers=displayPageNUmbers(pageCount, currentPage, name);
+    
+
+
     document.querySelector('#paginator').innerHTML = /*html */ pageCount > 1 ?`
         <div class="flex justify-center gap-1 py-4 text-xl">
 
             ${PaginationButton('prev', prevPage, name)}
-            ${PaginationButton('1', 1, name)}
-            ${currentPage > 1 && currentPage < pageCount ? PaginationButton(currentPage, currentPage, name) : '' }
-            ${PaginationButton(pageCount, pageCount, name)}
+            ${pageNUmbers}
             ${PaginationButton('next', nextPage, name)}
         </div>
     `: '';
+}
+
+const displayPageNUmbers = (pageCount, currentPage, name="") => {
+    let pageNumbers = [];
+    if(pageCount <= 5) {
+        return Array(5).fill().map((_, index) => PaginationButton(index+1, index+1, name)).join('');
+    }
+
+    if(currentPage <3) {
+        pageNumbers = [...Array(3).fill().map((_, index) => PaginationButton(index+1, index+1, name))]
+        pageNumbers.push('<div>...</div>')
+        pageNumbers.push(PaginationButton(pageCount, pageCount, name))
+        return pageNumbers.join('');
+    }
+
+
+    if(currentPage > pageCount - 2) {
+        pageNumbers.push(PaginationButton('1', 1, name))
+        pageNumbers.push('<div>...</div>')
+        pageNumbers = [...pageNumbers, ...Array(3).fill().map((_, index) => PaginationButton(pageCount + index- 2, pageCount + index- 2, name))]
+
+        return pageNumbers.join('');
+    }
+
+    pageNumbers.push(PaginationButton('1', 1, name))
+    if(currentPage !== 3) {
+        pageNumbers.push('<div>...</div>')
+    }
+    pageNumbers.push(PaginationButton(currentPage - 1, currentPage - 1, name))
+    pageNumbers.push(PaginationButton(currentPage, currentPage, name))
+    pageNumbers.push(PaginationButton(currentPage + 1, currentPage + 1, name))
+    if(currentPage !== pageCount - 2) {
+        pageNumbers.push('<div>...</div>')
+    }
+    pageNumbers.push(PaginationButton(pageCount, pageCount, name))
+
+    return pageNumbers.join('');
 }
